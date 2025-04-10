2cd1b4bb17ae5b9fa8633b73d7a6cf6c4c7e2e25
Leo <l.castellon01@ufromail.cl>
2025-04-10 10:03:08 -0400

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-        public static void main(String[] args) {
-            System.out.println("Bienvenido al menú de control");
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al menú de control");
 
-            System.out.println("1. Crear matriz");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
+        System.out.println("1. Crear matriz");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. ");
 
-            System.out.println("6. ");
+        System.out.println("6. ");
+    }
+
+    public static int[][] crearMatriz(int filas, int cols) {
+        return new int[filas][cols];
+    }
+
+    public static void LlennarMatriz(int[][] matriz, int filas, int cols) {
+        Random r = new Random();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j< cols; j++) {
+                matriz[i][j] = r.nextInt(10);
+            }
         }
 
-        public static int PedirDatos{
 
     }
-        public static int[][] CrearMatriz(int filas, int cols) {
-            return
-
-        }
-
-        public static boolean validarDimensiones(int filas, int cols) {
-
-        }
-
-    }
+}

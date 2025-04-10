0948d4ef8993cd47033a897fdc7fe80689db543d
Leo <l.castellon01@ufromail.cl>
2025-04-10 09:35:06 -0400

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+        public static void main(String[] args) {
+            System.out.println("Bienvenido al menú de control");
+
+            System.out.println("1. Crear matriz");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("5. ");
+
+            System.out.println("6. ");
         }
+
+        public static int PedirDatos{
+
     }
-}
+        public static int[][] CrearMatriz(int filas, int cols) {
+            return
+
+        }
+
+        public static boolean validarDimensiones(int filas, int cols) {
+
+        }
+
+    }
